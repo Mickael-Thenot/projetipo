4156468ffb425759f0b0befe161969344aba7e08
Mickael-Thenot <mickael@thenot.eu>
2020-11-24 10:22:21 +0100

    Partie 2 : attrbuts, constructeurs et implémentations de méthodes où on commence à créer notre propre environnement.

---
+++ squelPartie2/frogSquel2/src/environment/environment/Environment.java
@@ -0,0 +1,51 @@
+package environment;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import givenEnvironment.Lane;
+import util.Case;
+import gameCommons.Game;
+import gameCommons.IEnvironment;
+
+public class Environment implements IEnvironment {
+
+    //TODO
+
+    private ArrayList<Lane> roadLines;
+    private Game game;
+
+    public Environment(Game game) {
+        this.game = game;
+        this.roadLines = new ArrayList();
+        this.roadLines.add(new Lane(game, 0, 0.0D));
+
+        for(int i = 1; i < game.height - 1; ++i) {
+            this.roadLines.add(new Lane(game, i));
+        }
+
+        this.roadLines.add(new Lane(game, game.height - 1, 0.0D));
+    }
+
+
+    @Override
+    public boolean isSafe(Case c) {
+        return ((Lane)this.roadLines.get(c.ord)).isSafe(c);
+
+    }
+
+    @Override
+    public boolean isWinningPosition(Case c) {
+        return false;
+    }
+
+    @Override
+    public void update() {
+        Iterator var2 = this.roadLines.iterator();
+
+        while(var2.hasNext()) {
+            Lane lane = (Lane)var2.next();
+            lane.update();
+        }
+    }
+}
